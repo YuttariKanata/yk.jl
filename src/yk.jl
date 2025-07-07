c6ef049deb872bb4c2c015b582a2cb0fd892238e
YuttariKanata <yuttarikanata@gmail.com>
2025-07-08 05:30:29 +0900

    iroot

--- src/yk.jl
+++ src/yk.jl
@@ -1,5 +1,14 @@
 module yk
 
-# Write your package code here.
+import Base.GMP:libgmp
+import Base.GMP.MPZ.mpz_t
+
+
+function iroot(x::BigInt,n::UInt32)
+    x < 0 && throw(DomainError(x, "iroot(x, n) is undefined for negative x"))
+    z = BigInt()
+    ccall((:__gmpz_root, libgmp), Cint, (mpz_t, mpz_t, Culong), z, x, n)
+    return z
+end
 
 end
